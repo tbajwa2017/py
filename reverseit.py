d12a5bfd287a1e7b8abf9f3501b2e5e66e8cb141
tbajwa2017 <tbajwa@gmail.com>
2018-04-02 20:00:20 -0400

    adding reverseit.py

---
+++ reverseit.py
@@ -0,0 +1,14 @@
+name1 = "anna"
+
+
+#print len(name1)
+num1=len(name1)
+str2 = name1[0]
+str1 = ""
+for i in range(num1-1, -1, -1):
+   str1 = str1 + name1[i]
+#str1 = str1 + str2
+#print str1
+print str1
+ 
+
