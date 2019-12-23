0bdfed90394d29b2b4c5aa7c76121f9855ba7ee9
root <root@vm1.my-domain123.com>
2019-12-23 11:18:17 -0500

    Adding mem1.c

---
+++ mem1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+
+{
+
+char * char_ptr ;
+
+char_ptr = (char *)calloc(268435456 , sizeof(char)) ;
+if ( char_ptr == NULL )
+{
+printf("Memory not allocated and inititalized.\n") ;
+exit(100) ;
+}
+
+
+/*memcpy(char_ptr,"C",strlen(char_ptr)) ;
+*/
+printf("%d\n", strlen(char_ptr)) ;
+while (1)
+{
+sleep(100);
+}
+
+
+
+return (0) ;
+
+}
+
+
